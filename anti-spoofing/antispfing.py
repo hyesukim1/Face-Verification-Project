357017fbe50619d18d482119e9a18648b12ca5a9
hyesukim1 <94281700+hyesukim1@users.noreply.github.com>
2022-07-29 18:02:01 +0900

    22.07.29

--- anti-spoofing/antispfing.py
+++ anti-spoofing/antispfing.py
@@ -21,7 +21,7 @@ def calc_hist(img):
 while True:
     ret, frame = cap.read()
     if ret is False:
-        print "Error happens from camera"
+        print ("Error happens from camera")
 
     frame = cv2.flip(frame, 1)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -37,11 +37,34 @@ while True:
         ycrcb_hist = calc_hist(img_ycrcb)
         luv_hist = calc_hist(img_luv)
 
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        feature_vector = np.append(ycrcb_hist.ravel(), luv_hist.ravel())
+        feature_vector = feature_vector.reshape(1, len(feature_vector))
+
+        prediction = clf.predict_proba(feature_vector)
+        prob = prediction[0][1]
+
+        measures[count % sample_number] = prob
+        print measures, np.mean(measures)
+
+        if 0 not in measures:
+            text = "True"
+            if np.mean(measures) >= 0.7:
+                text = "False"
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                cv2.putText(img=img_bgr, text=text, org=point, fontFace=font, fontScale=0.9, color=(0, 0, 255),
+                            thickness=2, lineType=cv2.LINE_AA)
+            else:
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                cv2.putText(img=img_bgr, text=text, org=point, fontFace=font, fontScale=0.9,
+                            color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
         
         cv2.imshow('camera', frame)
 
         key = cv2.waitKey(1)
-        if key & 0xFF == 27:
+
+        if cv2.waitKey(0) & 0xFF == ord('q'):
             break
 
     cap.release()
