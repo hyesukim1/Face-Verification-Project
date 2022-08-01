952adf22b622fd7e2e23b99ba61d09bf53ab576a
nvidia <rlagt7246@naver.com>
2022-08-01 14:30:09 +0800

    22.08.01

--- face_blur.py
+++ face_blur.py
@@ -3,9 +3,11 @@ import cv2
 # import face_recognition
 import os
 #practice
+
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
+
 
 face_detector = cv2.CascadeClassifier('haarcascade_frontface.xml')
 
@@ -16,7 +18,8 @@ def blurBoxes(image, boxes):
         sub = image[x:w, y:h]
 
         # apply GaussianBlur on cropped area
-        blur = cv2.GaussianBlur(sub,(0,0), 2)
+        blur = cv2.GaussianBlur(sub,(3,3),0)
+        # blur = cv2.blur(sub, (10,10))
 
         # paste blurred image on the original image
         image[x:w, y:h] = blur
@@ -34,7 +37,7 @@ while(True):
     faces = face_detector.detectMultiScale(RGB)
 
     frame_blur = blurBoxes(frame, faces)
-    print("Num of faces: ", str(len(faces)))
+    # print("Num of faces: ", str(len(faces)))
 
     # if len(faces):
     #     for (x,y,w,h) in faces:
